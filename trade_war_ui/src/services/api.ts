eb4bb17c752a8b7616941a57eb1fb909e244c636
madushankaf <madushankafonseka@live.com>
2025-09-01 13:45:42 +0530

    added API base URL as env var

--- trade_war_ui/src/services/api.ts
+++ trade_war_ui/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { GameModel, GameResult } from '../types/game';
 
-const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}` || 'http://localhost:5010';
+const API_BASE_URL = `${process.env.API_BASE_URL}` || 'http://localhost:5010';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
